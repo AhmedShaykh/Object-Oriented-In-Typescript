c8d25a0006bc7fe34b39b374f75b520ec3a0d736
AhmedShaykh <ahmed.shaykhwork@gmail.com>
2022-10-21 00:09:05 +0500

    Git Init

--- src/App.ts
+++ src/App.ts
@@ -1,1 +1,32 @@
-console.log("Object Oriented Programming Using Typescript!");
+console.log('Learning TS!');
+
+const abc: string = "TS";
+const num: number = 123;
+const bool: boolean = true;
+let undef: undefined;
+let nul: null = null;
+
+console.log(abc);
+console.log(num);
+console.log(bool);
+console.log(undef);
+console.log(nul);
+
+let firstName: string = "Ahmed";
+console.log(firstName.length);
+
+let boolean: boolean = Math.random() > 0.5 ? true : false;
+console.log(boolean);
+
+let nam: string = "Ahmed Shaykh";
+nam = "Ahmed Shaikh";
+
+console.log(nam);
+
+let rocket: string = "Rocket";
+console.log(rocket.toUpperCase());
+console.log(rocket.toLowerCase());
+
+let myName: string;
+myName = "Ahmed Saleem Shaikh";
+console.log(myName);
