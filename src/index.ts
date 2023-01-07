e6d1cc223a68896add2ea2bc2a817289b9a80d87
Ahmed Saleem Shaikh <ahmed.shaykhwork@gmail.com>
2023-01-08 01:35:05 +0500

    Git Init

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,12 @@
+class Admin {
+    fName: String = "Ahmed"; // Initial Value
+    mName!: String; // Optional Value
+    lName: String = "Shaikh";
+};
+
+const username = new Admin();
+console.log(`${username.fName} ${username.mName = "Saleem"} ${username.lName}`);
+
 class Data {
     name: String;
 
@@ -6,5 +15,46 @@ class Data {
     }
 };
 
-const show = new Data("Learning OOP In TypeScript");
+const show: Data = new Data("Learning OOP In TypeScript");
 console.log(show.name);
+
+class Human {
+    name: String;
+    food: String;
+
+    constructor(name: String, food: String) {
+        this.name = name;
+        this.food = food;
+    }
+
+    eat() {
+        console.log(`${this.name} is a Human & is Eating ${this.food}`);
+    }
+};
+
+const user1: Human = new Human("Bilal", "Biryani");
+console.log(user1);
+user1.eat();
+
+const user2: Human = new Human("Abrar", "Chips");
+user2.eat();
+
+class ReadData {
+    readonly name: String = "AHM X";
+};
+
+const read: ReadData = new ReadData();
+console.log(read.name);
+
+class Calculator {
+    add(x: number, y: number): number;
+
+    add(x: string, y: string): string;
+
+    add(x: any, y: any): any {
+        return x + y;
+    }
+};
+
+const cal = new Calculator();
+console.log(cal.add(4, 10));
